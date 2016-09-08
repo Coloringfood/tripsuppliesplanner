484346bea3764795a1465d4303dd0db75f8e87c7
Brent Hawley <brent.hawley@insidesales.com>
2016-09-08 09:02:12 -0600

    Updating accessControll

--- devServer.js
+++ devServer.js
@@ -10,8 +10,8 @@ var compiler = webpack(config);
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', "*");
-    res.header('Access-Control-Allow-Headers', "X-Requested-With, Content-Type");
+    res.header('Access-Control-Allow-Methods', "GET,PUT,POST,DELETE");
+    res.header('Access-Control-Allow-Headers', "Content-Type");
 
     console.log(req.method + " " + req.originalUrl);
     next();
