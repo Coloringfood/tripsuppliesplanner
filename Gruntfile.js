b5d32d8cfd9899299d3fb19fe9bd831697e5f146
Brent Hawley <brent.hawley@insidesales.com>
2016-09-14 12:40:24 -0600

    cleaning gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,62 +70,6 @@ module.exports = function (grunt) {
             build: {}
         },
 
-        scsslint: {
-            allFiles: files.sass.all,
-            options: {
-                config: 'config/scss-lint.yml',
-                reporterOutput: 'build/code/lint/sass/checkstyle.xml'
-            }
-        },
-
-        jshint: {
-            all: {
-                src: [files.js.all, 'Gruntfile.js', files.js.not.autogen, files.js.not.external],
-                options: {
-                    globals: ['require'],
-                    "jasmine": true,
-                    "node": true,
-                    "predef": [
-                        'element',
-                        'browser',
-                        'by',
-                        'By',
-                        'angular'
-                    ]
-                }
-            },
-            report: {
-                src: [files.js.all, 'Gruntfile.js', files.js.not.autogen, files.js.not.external],
-                options: {
-                    globals: ['require'],
-                    "jasmine": true,
-                    "node": true,
-                    "predef": [
-                        'element',
-                        'browser',
-                        'by',
-                        'By',
-                        'angular'
-                    ],
-                    reporter: 'checkstyle',
-                    reporterOutput: 'build/code/lint/js/checkstyle.xml'
-                }
-            }
-        },
-
-        yuidoc: {
-            all: {
-                name: '<%= pkg.name %>',
-                description: '<%= pkg.description %>',
-                version: '<%= pkg.version %>',
-                options: {
-                    paths: files.js.dir,
-                    themedir: 'node_modules/grunt-contrib-yuidoc/node_modules/yuidocjs/themes/simple/',
-                    outdir: 'build/documentation/code/js'
-                }
-            }
-        },
-
         mkdir: {
             build: {
                 options: {
@@ -161,44 +105,6 @@ module.exports = function (grunt) {
             }
         },
 
-        uglify: {
-            prod: {
-                files: {
-                    'views/Powerdialer.min.js': [
-                        'views/**/*.js'
-                    ]
-                }
-            }
-        },
-
-        htmlbuild: {
-            src: 'views/index.html',
-            options: {
-                beautify: true,
-                replace: true,
-                prefix: '/',
-                scripts: {
-                    vendor: '<%= prod_config.SCRIPTS.VENDOR %>',
-                    app: '<%= prod_config.SCRIPTS.APP %>'
-                },
-                styles: {
-                    vendor: '<%= prod_config.STYLES.VENDOR %>',
-                    app: '<%= prod_config.STYLES.APP %>'
-                }
-            }
-        },
-
-        nggettext_extract: {
-            pot: {
-                files: {
-                    'assets/translations/po/template.pot': [
-                        '*/views/**/*.html',
-                        '*/views/**/*.js'
-                    ]
-                }
-            }
-        },
-
         nggettext_compile: {
             all: {
                 options: {
@@ -227,75 +133,6 @@ module.exports = function (grunt) {
             dist: {
                 src: 'assets/css/base.css'
             }
-        },
-
-        ngtemplates: {
-            salesindicatorApp: {
-                src: [
-                    'views/**/*.html'
-                ],
-                dest: 'common/pdTemplates.js',
-                options: {
-                    prefix: '/PowerDialer/',
-                    htmlmin: {collapseWhitespace: true}
-                }
-            }
-        },
-
-        htmlangular: {
-            templates: {
-                options: {
-                    tmplext: 'html',
-                    customtags: [
-                        'is-accordion',
-                        'is-messages-display',
-                        'is-nav-content',
-                        'is-nav-header',
-                        'is-nav-side',
-                        'is-table',
-                        'is-select'
-                    ],
-                    customattrs: [
-                        'is-submit',
-                        'is-submit-error'
-                    ],
-                    relaxerror: [
-                        'The “name” attribute is obsolete. Consider putting an “id” attribute on the nearest container instead.',
-                        'Bad value “{{ currentMaxPages }}” for attribute “max” on element “input”: Expected a minus sign or a digit but saw “{” instead.',
-                        'Element “img” is missing required attribute “src”.',
-                        'A table row was 12 columns wide and exceeded the column count established by the first row (11).',
-                        'A table row was 6 columns wide and exceeded the column count established by the first row (1).',
-                        'Table columns in range 2…6 established by element “td” have no cells beginning in them.',
-                        'A table row was 6 columns wide and exceeded the column count established by the first row (2).',
-                        'Table columns in range 3…6 established by element “td” have no cells beginning in them.',
-                        'Bad value “{{currentMaxPages}}” for attribute “max” on element “input”: Expected a minus' +
-                        ' sign or a digit but saw “{” instead.',
-                        'Attribute “analytics-on” not allowed on element “button” at this point.',
-                        'Attribute “analytics-category” not allowed on element “button” at this point.',
-                        'Attribute “analytics-event” not allowed on element “button” at this point.'
-                    ]
-                },
-                files: {
-                    src: [
-                        'views/dialerlist/**/*.html'
-                    ]
-                }
-            },
-            main: {
-                options: {
-                    customtags: [
-                        'si-message'
-                    ],
-                    relaxerror: [
-                        'Attribute “si-mixpanel” not allowed on element “body” at this point.'
-                    ]
-                },
-                files: {
-                    src: [
-                        'views/*.html'
-                    ]
-                }
-            }
         }
     });
 
@@ -311,6 +148,6 @@ module.exports = function (grunt) {
     grunt.registerTask('prepare', ['mkdir:build']);
 
     require('load-grunt-tasks')(grunt, {
-        pattern: ['grunt-*', '!grunt-template-jasmine-istanbul', '!grunt-template-jasmine-requirejs']
+        pattern: ['grunt-*']
     });
 };
