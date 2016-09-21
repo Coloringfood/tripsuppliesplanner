4dd589a38e226025f07b5e51e35ac03d8f287215
Brent Hawley <brent.hawley@insidesales.com>
2016-09-21 10:00:45 -0600

    Added Ability to edit Name of Items and save it

--- public/tripsuppliesplanner/controllers/EditItemController.modal.js
+++ public/tripsuppliesplanner/controllers/EditItemController.modal.js
@@ -3,14 +3,22 @@ powerdialerApp.controller('EditItemModalController',
         '$scope',
         '$uibModalInstance',
         'item',
-        function ($scope, $uibModalInstance, item) {
+        'DialerListApiService',
+        function ($scope, $uibModalInstance, item, DialerListApiService) {
             'use strict';
             var vm = this;
 
             vm.item = item;
+            vm.newItem = {
+                name: item.name,
+                categories: item.categories
+            };
 
             vm.ok = function () {
-                $uibModalInstance.close("Ok Clicked");
+                DialerListApiService.saveItem(vm.newItem, vm.item.id)
+                    .then(function (result){
+                        $uibModalInstance.close("Item Saved");
+                    });
             };
 
             vm.cancel = function () {
